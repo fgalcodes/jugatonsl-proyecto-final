d35a7a48b6a8001a6da9026d4a0608d6910c0c9d
adrian.ontiveros <adrian.ontiveros.7e4@itb.cat>
2023-05-05 10:22:40 +0200

    retoques

---
+++ Website/scripts/main.js
@@ -0,0 +1,4 @@
+// let footer = document.querySelector(".mainFooter");
+// footer.style.position = "absolute";
+// footer.style.width = "100%";
+// footer.style.bottom = screen.height - footer.height;
