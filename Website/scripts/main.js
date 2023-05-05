0dad9869247ac9f7158fb6bdeebbea07e4809553
DAMv1 <DAMv1@MAQUETA>
2023-05-05 09:12:57 +0200

    Fetch y demas

---
+++ Website/scripts/main.js
@@ -0,0 +1,17 @@
+const url = "https://localhost:7041/api/Usuarios";
+
+fetch(url)
+  .then(response => {
+    //handle response            
+    console.log(response);
+    if (response.ok) {
+        return response.json();
+    }
+  })
+  .then(data => {
+    //handle data
+    console.log(data);
+  })
+  .catch(error => {
+    //handle error
+  });
