f26ce85a711d60a757cbbc08594de90404b9f3de
DAMv1 <DAMv1@MAQUETA>
2023-05-08 12:49:18 +0200

    levels y merge

--- Website/scripts/levels.js
+++ Website/scripts/levels.js
@@ -1,1 +1,22 @@
-const apiUsuarios = "https://localhost:7041/api/Usuarios";
+const usuario21 = "https://localhost:7041/api/Perfiles/21";
+
+fetch(usuario21)
+  .then((response) => {
+    //handle response
+    console.log(response);
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    //handle data
+    console.log(data);
+    if (data.nivel === 1) {
+      console.log("Campeón has entrado al nivel 1");
+      alert("Campeón has entrado al nivel 1");
+    }
+  })
+  .catch((error) => {
+    //handle error
+    console.log(error);
+  });
